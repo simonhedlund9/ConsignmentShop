f0483be4efdccd461b57168d2590534a2df5d2d3
Simon Hedlund <sihe0008@student.hv.se>
2021-09-22 15:13:05 +0200

    Add project files.

---
+++ ConsignmentShop.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ConsignmentShop
+{
+    partial class ConsignmentShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerText = new System.Windows.Forms.Label();
+            this.itemsListbox = new System.Windows.Forms.ListBox();
+            this.itemsListboxLabel = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // headerText
+            // 
+            this.headerText.AutoSize = true;
+            this.headerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerText.Location = new System.Drawing.Point(56, 40);
+            this.headerText.Name = "headerText";
+            this.headerText.Size = new System.Drawing.Size(384, 37);
+            this.headerText.TabIndex = 0;
+            this.headerText.Text = "Consignment Shop Demo";
+            // 
+            // itemsListbox
+            // 
+            this.itemsListbox.FormattingEnabled = true;
+            this.itemsListbox.ItemHeight = 20;
+            this.itemsListbox.Location = new System.Drawing.Point(63, 121);
+            this.itemsListbox.Name = "itemsListbox";
+            this.itemsListbox.Size = new System.Drawing.Size(369, 224);
+            this.itemsListbox.TabIndex = 1;
+            // 
+            // itemsListboxLabel
+            // 
+            this.itemsListboxLabel.AutoSize = true;
+            this.itemsListboxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemsListboxLabel.Location = new System.Drawing.Point(59, 88);
+            this.itemsListboxLabel.Name = "itemsListboxLabel";
+            this.itemsListboxLabel.Size = new System.Drawing.Size(103, 20);
+            this.itemsListboxLabel.TabIndex = 2;
+            this.itemsListboxLabel.Text = "Store Items";
+            // 
+            // addToCart
+            // 
+            this.addToCart.Location = new System.Drawing.Point(304, 351);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(128, 49);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "Add To Cart";
+            this.addToCart.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(718, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Store Items";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(722, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(369, 224);
+            this.listBox1.TabIndex = 4;
+            // 
+            // ConsignmentShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 570);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.itemsListboxLabel);
+            this.Controls.Add(this.itemsListbox);
+            this.Controls.Add(this.headerText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ConsignmentShop";
+            this.Text = "Consignment Shop Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.ListBox itemsListbox;
+        private System.Windows.Forms.Label itemsListboxLabel;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
